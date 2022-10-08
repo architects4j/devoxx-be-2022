8657247089d5b00f9a2198721d81ed0c0f76f5c4
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-08 16:30:37 +0100

    Create body test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- cdi-se/src/test/java/org/a4j/presentation/ultrafast/GalaxyTest.java
+++ cdi-se/src/test/java/org/a4j/presentation/ultrafast/GalaxyTest.java
@@ -1,7 +1,28 @@
 package org.a4j.presentation.ultrafast;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GalaxyTest {
 
+    private Galaxy galaxy;
+
+    @BeforeEach
+    public void setUp() {
+        this.galaxy = new Galaxy();
+    }
+
+    @Test
+    public void shouldReturnErrorWhenAddNullBody() {
+        Assertions.assertThrows(NullPointerException.class, () ->
+                galaxy.add(null));
+    }
+
+    @Test
+    public void shouldAddBody() {
+
+    }
 }
