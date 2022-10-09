5ccc0c517a4202e673d2b2e11c805a6565303608
Otavio Santana <otaviopolianasantana@gmail.com>
2022-10-09 07:35:17 +0100

    Add team resource
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- microprofile/src/main/java/org/a4j/presentation/ultrafast/TeamResource.java
+++ microprofile/src/main/java/org/a4j/presentation/ultrafast/TeamResource.java
@@ -2,6 +2,7 @@ package org.a4j.presentation.ultrafast;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -34,5 +35,10 @@ public class TeamResource {
                 .orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
     }
 
+    @DELETE
+    @Path("{name}")
+    public void deleteById(@PathParam("name") String name) {
+        this.service.delete(name);
+    }
 
 }
